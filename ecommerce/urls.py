698ac94a2c195f2428cb88f024eb9937899296db
Saugat <saugat.balami@gmail.com>
2023-06-19 15:57:38 +0545

    Your commit message

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,7 +20,8 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from products.views import CheckoutSessionView
-
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(
@@ -49,4 +50,6 @@ urlpatterns = [
         CheckoutSessionView.as_view(),
         name="create-checkout-session",
     ),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
